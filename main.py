d71e758a3f805f857e491e7df14b5dc48e7322e8
Jason Deano <jmdeano@gmail.com>
2021-05-17 01:15:04 -0400

    feat: :sparkles: implement commands.py

--- main.py
+++ main.py
@@ -14,16 +14,13 @@ from rich.panel import Panel
 from rich.rule import Rule
 from rich.table import Table
 
-from CRIT import HEADERS, __version__, character
-from CRIT.attribute import Attribute
+from CRIT import HEADERS, __version__
 from CRIT.charutils import CharUtils
+from CRIT.commands import Commands
 from CRIT.compat import clear, pause, set_terminal_size, set_terminal_title
 from CRIT.config import Config
 from CRIT.enums import Enums
-from CRIT.item import Item
 from CRIT.json_parser import JsonParser
-from CRIT.save import Save
-from CRIT.spell import Spell
 from CRIT.utils import Utils
 
 if platform.system() == 'Windows':
@@ -238,8 +235,10 @@ class TUI:
         if self.first_update == False:
             return()
         sheet_to_load = click.prompt('which sheet should we load', type=Utils.get_options_from_dir(Config.sheets_path))
+        sheet_to_load = Path(Config.sheets_path, f'CRIT{sheet_to_load}.json'.replace(' ', '_'))
         self.console.print('get character')
-        self.character = JsonParser.load_character(Path(Config.sheets_path, f'CRIT{sheet_to_load}.json'.replace(' ', '_')))
+        self.character = JsonParser.load_character(sheet_to_load)
+        self.loaded_sheet = sheet_to_load
         self.console.print('setup attributes')
         CharUtils.setup_attributes(self.character)
         self.console.print('setup saves')
@@ -257,6 +256,17 @@ class TUI:
         }))
 
     # post load
+    def c_cast(self, args):
+        spell_list = []
+        for spell in self.character.spell_list:
+            if spell.base != 0:
+                spell_list.append(int(spell.level))
+        min_level = min(spell_list)
+        max_level = max(spell_list)
+        spell_level = click.prompt('what level spell?', type=click.IntRange(min_level, max_level))
+        Commands.cast(self.character, spell_level)
+        self.update()
+
     def c_heal(self, args):
         if not args:
             heal_value = click.prompt('How much did you heal?', type=int)
@@ -285,63 +295,24 @@ class TUI:
         self.update()
 
     def c_modify(self, args):
-        modify_option = click.prompt('modifiable elements:', type= click.Choice(['user', 'maxhp', 'skill', 'attribute'], case_sensitive=False))
+        if not args:
+            modify_option = click.prompt('modifiable elements:', type= click.Choice(['user', 'maxhp', 'skill', 'attribute'], case_sensitive=False))
+        else:
+            try:
+                modify_option = args
+            except:
+                pass
         if modify_option == 'user':
-            user_option = click.prompt('add or modify user element', type=click.Choice(['add', 'modify'], case_sensitive=False))
-            if user_option == 'add':
-                with open (self.loaded_sheet, 'r') as f:
-                    existing_data = json.load(f)
-                    key_to_add = click.prompt('what to add to user space (Only use if you know what you\'re doing)', type=click.STRING)
-                    existing_data['usr'][key_to_add] = []
-                with open (self.loaded_sheet, 'w+') as outfile:
-                    json.dump(existing_data, outfile, indent=4)
-                self.console.print(f'[bold green] UPDATED {self.character.name} [/bold green]')
-            elif user_option == 'modify':
-                with open (self.loaded_sheet, 'r') as f:
-                    existing_data = json.load(f)
-                    values = existing_data['usr'].keys()
-                    attr_to_update = click.prompt('what to update (Only use if you know what you\'re doing)', type=click.Choice(values, case_sensitive=False))
-                    value_to_add = click.prompt('what value should we add', type=click.STRING)
-                    existing_data['usr'][attr_to_update].append(value_to_add)
-                with open (self.loaded_sheet, 'w+') as outfile:
-                    json.dump(existing_data, outfile, indent=4)
-                self.console.print(f'[bold green] UPDATED {self.character.name} [/bold green]')
-            else:
-                self.console.print('enter valid option')
+            Commands.modify_user(self.character, self.loaded_sheet, self.console)
         if modify_option == 'maxhp':
-            maxhp = click.prompt('new max hp?', type=int)
-            self.character.max_hp = maxhp
+            Commands.modify_maxhp(self.character)
         if modify_option == 'skill':
-            skill_list = []
-            for skill in self.character.skill_list:
-                skill_list.append(skill.name)
-            skill_to_modify = click.prompt('what skill?', type=click.Choice(skill_list, case_sensitive=False))
-            for skill in self.character.skill_list:
-                if skill.name == skill_to_modify:
-                    skill_option = click.prompt(f'current skill rank is {skill.rank}, bonus is {skill.bonus}, for a total of {skill.total}. change rank or bonus?', type=click.Choice(['rank', 'bonus'], case_sensitive=False))
-                    if skill_option == 'rank':
-                        new_rank = click.prompt('what is the new skill rank?', type=int)
-                        skill.rank = new_rank
-                    if skill_option == 'bonus':
-                        new_bonus = click.prompt('what is the new skill bonus?', type=int)
-                        skill.bonus = new_bonus
+            Commands.modify_skills(self.character)
         if modify_option == 'attribute':
-            character.attr_list = []
-            for attr in self.character.attr_list:
-                character.attr_list.append(attr.name)
-            attr_to_modify = click.prompt('which attribute?', type=click.Choice(character.attr_list, case_sensitive=False))
-            for attr in self.character.attr_list:
-                if attr.name == attr_to_modify:
-                    attr_option = click.prompt(f'current attribute base is {attr.base}, bonus is {attr.bonus}, for a total of {attr.total}. change base or bonus?', type=click.Choice(['base', 'bonus'], case_sensitive=False))
-                    if attr_option == 'base':
-                        new_base = click.prompt('what is the new attribute base', type=int)
-                        attr.base = new_base
-                    if attr_option == 'bonus':
-                        new_bonus = click.prompt('what is the new attribute bonus', type=int)
-                        attr.bonus = new_bonus
+            Commands.modify_attribute(self.character)
         self.update()
 
-    def c_save(self, args):
+    def c_save(self):
         JsonParser.save_character(self.character)
 
     def c_level(self, args):
@@ -353,162 +324,60 @@ class TUI:
             except:
                 self.console.print('please enter an int')
                 return()
-
-        self.character.level = char_level
-        char_level = char_level-1
-        with open(Path(Config.data_path, 'classes', f'{self.character.class_}.json'), 'r') as f:
-            class_config = json.load(f)
-            self.character.bab = class_config['bab'][char_level]
-            # up saves
-            for sav in self.character.save_list:
-                sav.base = class_config[sav.name][char_level]
-            # up spells 
-            self.console.print(self.character.spell_list)
-            self.console.print(class_config['spells'])
-            if self.character.spell_list:
-                self.character.spell_list = []
-                for level, spell in class_config['spells'].items():
-                    self.console.print(f'level {level} spell {spell}')
-                    self.character.spell_list.append(Spell(level = int(level)
-                                                    , save = 0
-                                                    , slots = spell[char_level]
-                                                    , remaining = spell[char_level]
-                                                    , base = spell[char_level]
-                                                    ))
-
+        Commands.level(self.character, self.console, char_level)
         self.update()
         self.console.print('Remember to add your HP with the modify command')
 
     def c_item(self, args):
         modify_option = click.prompt('', type=click.Choice(['list', 'add', 'remove', 'equip', 'unequip'], case_sensitive=False))
         if modify_option == 'list':
-            for item in self.character.item_list:
-                self.console.print(item)
+            Commands.item_list(self.character, self.console)
+
         if modify_option == 'add':
-            item_name = click.prompt('what is the item\'s name', type=str)
-            acp = 0
-            ac_type = None
-            dex_mod = 0
-            item_slots = []
-            item_slot = click.prompt('what slot is the item in?', type=click.Choice(Enums.gear_slots, case_sensitive=False))
-            if click.confirm('is there a second slot?'):
-                item_slot2 = click.prompt('what second slot is the item in?', type=click.Choice(Enums.gear_slots, case_sensitive=False))
-                if item_slot != item_slot2:
-                    item_slots.append(item_slot2)
-            item_slots.append(item_slot)
-            item_ac = click.prompt('how much ac does the item provide', type=int)
-            if item_ac != 0:
-                acp = click.prompt('what is the acp of the item?', type=click.IntRange(-9,0))
-                ac_type = click.prompt('what type of ac bonus?', type=click.Choice(Enums.bonus_types, case_sensitive=False))
-                dex_mod =  click.prompt('what is the max dex bonus (-1 for unlimited or N/A)?', type=click.IntRange(-1,9))
-            more = click.prompt('does the item modify anything else?', type=click.Choice(['attribute', 'save', 'skill', 'no'], case_sensitive=False))
-            bonus = {}
-            while more != 'no':
-                if more == 'attribute':
-                    item_attr = click.prompt(f'what {more} does the item modify?', type=click.Choice(Enums.attributes, case_sensitive=False))
-                if more == 'save':
-                    item_attr = click.prompt(f'what {more} does the item modify?', type=click.Choice(['fortitude', 'reflex', 'will'], case_sensitive=False))
-                if more == 'skill':
-                    skill_list = []
-                    for skill in self.character.skill_list:
-                        skill_list.append(skill.name)
-                    item_attr = click.prompt(f'what {more} does the item modify?', type=click.Choice(skill_list, case_sensitive=False))
-                item_attr_value = click.prompt(f'how much?', type=click.IntRange(-99,99))
-                item_attr_type = click.prompt('what type of bonus', type=click.Choice(Enums.bonus_types, case_sensitive=False))
-                bonus[item_attr] = {'stat': item_attr
-                                , 'value':  item_attr_value
-                                , 'type': item_attr_type}
-                more = click.prompt('does the item modify anything else?', type=click.Choice(['attribute', 'save', 'skill', 'no'], case_sensitive=False))
+            Commands.item_add(self.character)
 
-            new_item = Item(name = item_name
-                                    , equipped = False
-                                    , slot = item_slots
-                                    , ac = item_ac
-                                    , acp = acp
-                                    , dex_mod = dex_mod
-                                    , ac_type = ac_type
-                                    , bonus = bonus)
-            if click.confirm(f'create {new_item}?'):
-                self.character.item_list.append(new_item)
         if modify_option == 'remove':
-            item_list = []
-            for item in self.character.item_list:
-                item_list.append(item.name)
-            
-            item_to_remove = click.prompt('remove which item', type=click.Choice(item_list, case_sensitive=False))
+            Commands.item_remove(self.character, self.console)
 
-            for item in self.character.item_list:
-                if item.name == item_to_remove:
-                    self.console.print(f'removing {item_to_remove}')
-                    self.character.item_list.remove(item)
         if modify_option == 'equip':
-            item_list = []
-            for item in self.character.item_list:
-                if item.equipped == False:
-                    item_list.append(item.name)
+            Commands.item_equip(self.character, self.console)
+            self.update()
 
-            if item_list == []:
-                self.console.print('no items to equip')
-                return()
-            
-            item_to_equip_name = click.prompt('equip which item?', type=click.Choice(item_list, case_sensitive=False))
-            item_to_equip = {}
-            for item in self.character.item_list:
-                if item.name == item_to_equip_name:
-                    item_to_equip = item
-            
-            if 'slotless' in item_to_equip.slot:
-                pass
-            else:
-                equipped_items = []
-                for item in self.character.item_list:
-                    if item.equipped == True:
-                        equipped_items.append(item)
-                
-                if (type(item_to_equip.slot) is str):
-                    for item in equipped_items:
-                        if (type(item.slot) is str):
-                            if item_to_equip.slot == item.slot:
-                                self.console.print(f'unequip {item} first')
-                                return()
-                        if (type(item.slot) is list):
-                            for slot in item.slot:
-                                if item_to_equip.slot == slot:
-                                    self.console.print(f'unequip {item} first')
-                                    return()
-                if (type(item_to_equip.slot) is list):
-                    for item in equipped_items:
-                        if (type(item.slot) is str):
-                            if item.slot in item_to_equip.slot:
-                                self.console.print(f'unequip {item} first')
-                                return()
-                        if (type(item.slot) is list):
-                            for slot in item.slot:
-                                if slot in item_to_equip.slot:
-                                    self.console.print(f'unequip {item} first')
-                                    return()
-            
-            for item in self.character.item_list:
-                if item.name == item_to_equip.name:
-                    item.equipped = True
         if modify_option == 'unequip':
-            item_list = []
-            for item in self.character.item_list:
-                if item.equipped == True:
-                    item_list.append(item.name)
-            
-            if item_list == []:
-                self.console.print('no items to unequip')
-                return()
-
-            item_to_unequip_name = click.prompt('unequip which item?', type=click.Choice(item_list, case_sensitive=False))
-            for item in self.character.item_list:
-                if item.name == item_to_unequip_name:
-                    item.equipped = False
-        self.update()
+            Commands.item_unequip(self.character, self.console)
+            self.update()
 
     def c_feat(self, args):
-        pass
+        if not args:
+            feat_options = click.prompt('add or remove feat?', type= click.Choice(['add', 'remove', 'list'], case_sensitive=False))
+        else:
+            try:
+                feat_options = args
+            except:
+                pass
+
+        if feat_options == 'add':
+            Commands.feat_add(self.loaded_sheet)
+            self.console.print(f'[bold green] UPDATED {self.character.name} [/bold green]')
+
+        if feat_options == 'remove':
+            Commands.feat_remove(self.loaded_sheet)
+            self.console.print(f'[bold green] UPDATED {self.character.name} [/bold green]')
+        
+        if feat_options == 'list':
+            with open (self.loaded_sheet, 'r') as f:
+                existing_data = json.load(f)
+                grid = Table.grid(expand=True)
+                table = Table(box=box.ROUNDED, title='')
+                table.add_column('FEATS', justify='center',style='white',no_wrap=True)
+                for feat in existing_data['feats']:
+                    table.add_row(feat)
+                grid.add_row(table)
+            self.console.print(grid)
+
+    def c_rest(self, args):
+        Commands.rest(self.character)
+        self.update()
 
     # outputs
     def c_user(self, _):
@@ -525,7 +394,7 @@ class TUI:
 def cli():
     set_terminal_title(f'Character Resources In Terminal v{__version__}')
     click_completion.init()
-    os.chdir(os.path.dirname(os.path.abspath(sys.executable)))
+    # os.chdir(os.path.dirname(os.path.abspath(sys.executable)))
     app = TUI()
     app.start()
 
